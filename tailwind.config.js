6a18ebe2e5b77f8d674d96c9bdd6d50437a810c2
Logan Orr <logan_orr@icloud.com>
2023-03-23 14:41:58 -0600

    Server on index.js up and running

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+  content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
