2fee067406dfc4ff17d839fbccbb4a827331e814
Logan Orr <logan_orr@icloud.com>
2023-04-04 14:42:29 -0600

    Login and logout buttons now working and toggling

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,15 @@ import { Tracker } from './tracker/tracker'
 import { Login } from './login/login'
 import { AuthState } from './login/authState';
 import logo from './assets/tree.png'
+import { useNavigate } from 'react-router-dom';
 
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam9ycmxvZ2FuIiwiYSI6ImNsZnVqMTIydjAycTAzZ250bG1wc2xmc3cifQ.mZwH-XJARtUN7Ru4Y9N-mA';
 
 function App() {
+  const navigate = useNavigate();
+
   const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
 
   // Asynchronously determine if the user is authenticated by calling the service
@@ -38,6 +41,14 @@ function App() {
   }, [userName]);
 
 
+  function logout() {
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    })
+    .then(() => setAuthState(AuthState.Unauthenticated))
+  }
+
+
   return (
     <div>
 
@@ -48,7 +59,12 @@ function App() {
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Campsnatch</span>
           </NavLink>
           <div className="flex md:order-2">
-            <NavLink to="login" ><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button></NavLink>
+            {authState === AuthState.Unauthenticated && (
+              <NavLink to="login" ><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button></NavLink>
+            )}
+            {authState === AuthState.Authenticated && (
+              <NavLink to="login" ><button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={() => logout()}>Logout</button></NavLink>
+            )}
             <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
--- src/login/unathenticated.jsx
+++ src/login/unathenticated.jsx
@@ -34,32 +34,38 @@ export function Unauthenticated(props) {
 
   return (
     <>
-      <div>
-        <div className='input-group mb-3'>
-          <span className='input-group-text'>@</span>
-          <input
-            className='form-control'
-            type='text'
-            value={userName}
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder='your@email.com'
-          />
+      <div className="flex justify-start items-center content-center flex-col">
+        <div
+          className="md:w-5/12 h-3/6 flex justify-center content-center items-center flex-wrap flex-col p-8 rounded-lg bg-opacity-8 ">
+          <div className="mb-8">
+            <img src="./assets/fire-gif.gif" width="200px" />
+          </div>
+          <p className="md:text-7xl text-5xl font-black mb-8 text-white">Campsnatch</p>
+          <div className="w-7/12">
+            <div className="mb-3">
+              <label for="email" className="block mb-2 text-sm font-medium text-black dark:text-white">Email
+                address</label>
+              <input type="email" id="email"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="john.doe@company.com" required onChange={(e) => setUserName(e.target.value)}></input>
+            </div>
+            <div className="mb-6">
+              <label for="password"
+                className="block mb-2 text-sm font-medium text-black dark:text-white">Password</label>
+              <input type="password" id="password"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="•••••••••" required onChange={(e) => setPassword(e.target.value)}></input>
+            </div>
+            <div className="w-full flex justify-center">
+              <button type="submit"
+                className="focus:outline-none text-black bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:focus:ring-yellow-900"
+                onClick={() => loginUser()}>Log in</button>
+            </div>
+            <div id="error-message" className="flex justify-center">
+
+            </div>
+          </div>
         </div>
-        <div className='input-group mb-3'>
-          <span className='input-group-text'>🔒</span>
-          <input
-            className='form-control'
-            type='password'
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder='password'
-          />
-        </div>
-        <button variant='primary' onClick={() => loginUser()}>
-          Login
-        </button>
-        <button variant='secondary' onClick={() => createUser()}>
-          Create
-        </button>
       </div>
 
       {/* <MessageDialog message={displayError} onHide={() => setDisplayError(null)} /> */}
