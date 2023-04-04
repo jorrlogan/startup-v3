37d1cde5996676669829405eee99e7b3abab5f38
Logan Orr <logan_orr@icloud.com>
2023-04-04 13:46:02 -0600

    Trackers loading

--- src/tracker/tracker.jsx
+++ src/tracker/tracker.jsx
@@ -1,8 +1,129 @@
+import { async } from 'q'
 import React from 'react'
 
-export function Tracker(){
-    console.log('inside tracker')
+export function Tracker() {
+    const [tableHead, setTableHead] = React.useState(null)
+    const [trackerRows, setTrackerRows] = React.useState(null)
+
+
+    React.useEffect(() => {
+        const email = localStorage.getItem('userName')
+        console.log('email: ' + email)
+        if (email) {
+            getTrackers(email)
+        }
+    }, [])
+
+    async function getTrackers(email) {
+        let request = await fetch('/api/trackers', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                'device_token': `${email}`
+            })
+        })
+        let json = await request.json()
+        parseTrackers(json, 'logan')
+    }
+
+    function parseTrackers(json, device_token) {
+        console.log('parsing trackers')
+        let json_trackers = json.trackers
+        let trackers = []
+        for (const t of json_trackers) {
+            console.log(t.campground_id)
+            console.log(t.campground_name)
+            console.log(t.start_date)
+            console.log(t.end_date)
+            trackers.push(new Tracker(t.campground_id, t.campground_name, t.start_date, t.end_date, device_token))
+        }
+        displayTrackers(trackers)
+    }
+
+    class Tracker {
+        constructor(campground_id, campground_name, start_date, end_date, device_token) {
+            this.campground_id = campground_id
+            this.campground_name = campground_name
+            this.start_date = start_date
+            this.end_date = end_date
+            this.device_token = device_token
+        }
+    }
+
+    function displayTrackers(trackers) {
+        console.log('displaying trackers')
+        setTableHead(
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Campground Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Start Date
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    End Date
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    <span class="">Edit</span>
+                </th>
+            </tr>
+        )
+
+        let rows = []
+        for (const t of trackers) {
+            rows.push(
+                <tr>
+                    <th scope="row"
+                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                        {t.campground_name}
+                    </th>
+                    <td class="px-6 py-4 ">
+                        {t.start_date}
+                    </td>
+                    <td class="px-6 py-4">
+                        {t.end_date}
+                    </td>
+                    <td class="px-6 py-4 text-right">
+                        <a href="#"
+                            class="font-small text-blue-600 dark:text-blue-500 hover:underline" onclick="removeTracker('${t.device_token}', '${t.campground_id}', '${t.campground_name}', '${t.start_date}', '${t.end_date}')">
+                            <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" height="25px">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
+                            </svg>
+                        </a>
+                    </td>
+                </tr>
+            )
+        }
+        setTrackerRows(rows)
+    }
+
     return (
-        <h1>hello world</h1>
+        <div className="">
+            <div className="m-4 flex flex-col justify-center items-center">
+                <div className="w-9/12">
+                    <div className="relative shadow-md sm:rounded-lg rounded-lg bg-white overflow-x-auto">
+                        <table className="text-sm text-left text-gray-500 dark:text-gray-400 w-full">
+                            <thead id="tracker-table-head"
+                                className="text-xs  text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                {tableHead}
+                            </thead>
+                            <tbody id="tracker-table-body">
+                                {trackerRows}
+                            </tbody>
+                        </table>
+                        <div id="tracker-loader">
+
+                        </div>
+                    </div>
+
+                    <div id="tracker-messages">
+
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
