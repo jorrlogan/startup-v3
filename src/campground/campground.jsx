539b529c72555ab4d3401b7660d7a758300490f0
Logan Orr <logan_orr@icloud.com>
2023-04-04 20:10:49 -0600

    Tracker placement now correct

--- src/campground/campground.jsx
+++ src/campground/campground.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import './campground.css'
 import 'flowbite';
+import Datepicker from "flowbite-datepicker/Datepicker";
+import { Button, TextInput } from 'flowbite-react';
 
 export function Campground(props) {
     const { id, name } = useParams()
@@ -47,6 +49,12 @@ export function Campground(props) {
         }
     }, [])
 
+    React.useEffect(() => {
+        const datepickerEl = document?.getElementById("datepickerId");
+        // console.log(datepickerEl);
+        new Datepicker(datepickerEl, {});
+    }, []);
+
     return (
         <main>
             <div className="h-5/6 flex flex-row justify-center items-center flex-wrap">
@@ -70,28 +78,58 @@ export function Campground(props) {
                                         {name}
                                     </div>
                                     <div>
-                                        <div className='flex rounded-lg border p-1'>
-                                            <div class="relative max-w-sm border-r focus:ring-0">
-                                                <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                                    <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                                        {/* <div className='flex p-1'>
+                                            <div className='flex'>
+                                                <div className='flex rounded-lg border mr-4'>
+                                                    <div className="relative w-36 border-r p-1">
+                                                        <input
+                                                            datepicker
+                                                            datepicker-autohide
+                                                            type="text"
+                                                            className="text-gray-900 sm:text-sm border-none block w-full p-2.5 focus:ring-0 focus:ring-offset-0"
+                                                            placeholder="Select date"
+                                                            onSelect={(e) => console.log(e.target.value)}
+                                                            // onClick={(e) => dobHandler(e)}
+                                                            // onClick={(e) => console.log(e.target.value)}
+                                                            // onChange={(e) => console.log(e)}
+                                                            id="datepickerId"
+                                                        ></input>
+                                                        <div className="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
+                                                            <svg
+                                                                aria-hidden="true"
+                                                                class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                                                fill="currentColor"
+                                                                viewBox="0 0 20 20"
+                                                                xmlns="http://www.w3.org/2000/svg"
+                                                            >
+                                                                <path
+                                                                    fill-rule="evenodd"
+                                                                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                                                                    clip-rule="evenodd"
+                                                                ></path>
+                                                            </svg>
+                                                        </div>
+
+                                                    </div>
+                                                    <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button">Nights <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+
+                                                    <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                                                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                                                            {numbers.map((num) => (
+                                                                <li>
+                                                                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{num}</a>
+                                                                </li>
+                                                            ))}
+                                                        </ul>
+                                                    </div>
                                                 </div>
-                                                <input datepicker type="text" class="text-gray-900 text-sm block w-full pl-10 p-2.5 dark:text-white border-none focus:ring-0" placeholder="Select date"></input>
+                                                <div>
+                                                    <Button size="lg">
+                                                        Track
+                                                    </Button>
+                                                </div>
                                             </div>
-
-                                            <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-black focus:outline-none font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center" type="button">Nights <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-
-                                            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-                                                <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
-                                                    {numbers.map((num) => (
-                                                        <li>
-                                                            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">{num}</a>
-                                                        </li>
-                                                    ))}
-                                                </ul>
-                                            </div>
-
-                                        </div>
-
+                                        </div> */}
                                     </div>
                                 </div>
 
@@ -103,16 +141,58 @@ export function Campground(props) {
                                 </div>
                                 <div>
                                 </div>
-                                <div className='grid grid-cols-1 gap-4 mt-8'>
-                                    <div className="h-auto mb-24">
+                                <div className='flex mt-8'>
+                                    <div className="h-auto mb-24 w-3/4">
                                         <div id="campground-description" dangerouslySetInnerHTML={{ __html: campgroundDetails.facilityDescription }}>
                                         </div>
                                         <div id="campground-directions" dangerouslySetInnerHTML={{ __html: campgroundDetails.facilityDirections }}>
                                         </div>
                                     </div>
-                                    <div>
-                                        <div inline-datepicker></div>
+                                    <div className='flex p-1 w-1/4 pl-8'>
+                                        <div className='flex flex-col w-full'>
+                                            <div className='flex flex-col rounded-lg border mr-4 w-full mb-6'>
+                                                <div className="relative w-full border-b p-1">
+                                                    <input
+                                                        datepicker
+                                                        datepicker-autohide
+                                                        type="text"
+                                                        className="text-gray-900 sm:text-sm border-none block w-full p-2.5 focus:ring-0 focus:ring-offset-0"
+                                                        placeholder="Select date"
+                                                        onSelect={(e) => console.log(e.target.value)}
+                                                        // onClick={(e) => dobHandler(e)}
+                                                        // onClick={(e) => console.log(e.target.value)}
+                                                        // onChange={(e) => console.log(e)}
+                                                        id="datepickerId"
+                                                    ></input>
+                                                    <div className="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
+                                                        <svg
+                                                            aria-hidden="true"
+                                                            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                                            fill="currentColor"
+                                                            viewBox="0 0 20 20"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                        >
+                                                            <path
+                                                                fill-rule="evenodd"
+                                                                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                                                                clip-rule="evenodd"
+                                                            ></path>
+                                                        </svg>
+                                                    </div>
+
+                                                </div>
+                                                <div>
+                                                    <input type="number" id="first_name" class="text-gray-900 text-sm rounded-lg border-none focus:ring-0  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Num nights" required></input>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <Button size="lg" className='w-full'>
+                                                    Track
+                                                </Button>
+                                            </div>
+                                        </div>
                                     </div>
+
                                 </div>
                             </div>
                         </div>
