7c9f87814ade0cc06a260cfc05febaff2fbc7bd4
Logan Orr <logan_orr@icloud.com>
2023-04-05 14:04:11 -0600

    Spinner added for adding tracker

--- src/campground/campground.jsx
+++ src/campground/campground.jsx
@@ -7,7 +7,7 @@ import { Button, TextInput } from 'flowbite-react';
 import { Spinner } from 'flowbite-react';
 import { AuthState } from '../login/authState';
 
-export function Campground({authState}) {
+export function Campground({ authState }) {
     const { id, name } = useParams()
     const [campgroundDetails, setCampgroundDetails] = React.useState('')
     const [campgroundImages, setCampgroundImages] = React.useState([])
@@ -15,7 +15,9 @@ export function Campground({authState}) {
     const [date, setDate] = React.useState(null)
     const [nights, setNights] = React.useState(1)
     const [trackerItem, setTrackerItem] = React.useState(false)
-   console.log('authState:' + authState)
+    const [loading, setLoading] = React.useState(false)
+    const [trackerSet, setTrackerSet] = React.useState(false)
+    console.log('authState:' + authState)
 
     React.useEffect(() => {
 
@@ -45,7 +47,11 @@ export function Campground({authState}) {
                 })
             })
         }
+        if (trackerItem){
         setTracker()
+        .then(() => setLoading(false))
+        .then(() => setTrackerSet(true))
+        }
     }, [trackerItem])
 
     React.useEffect(() => {
@@ -135,52 +141,62 @@ export function Campground({authState}) {
                                             </div>
                                         </div>
                                         {authState === AuthState.Authenticated && (
-                                        <div className='flex p-1 lg:w-1/4 w-screen lg:pl-8'>
-                                            <div className='flex flex-col w-full'>
-                                                <div className='flex flex-col rounded-lg border mr-4 w-full mb-6'>
-                                                    <div className="relative w-full border-b p-1">
-                                                        <input
-                                                            datepicker
-                                                            datepicker-autohide
-                                                            type="text"
-                                                            className="text-gray-900 sm:text-sm border-none block w-full p-2.5 focus:ring-0 focus:ring-offset-0"
-                                                            placeholder="Select date"
-                                                            // onSelect={(e) => console.log(e.target.value)}
-                                                            onSelect={(e) => setDate(e.target.value)}
-                                                            // onClick={(e) => dobHandler(e)}
-                                                            // onClick={(e) => console.log(e.target.value)}
-                                                            // onChange={(e) => console.log(e)}
-                                                            id="datepickerId"
-                                                        ></input>
-                                                        <div className="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
-                                                            <svg
-                                                                aria-hidden="true"
-                                                                class="w-5 h-5 text-gray-500 dark:text-gray-400"
-                                                                fill="currentColor"
-                                                                viewBox="0 0 20 20"
-                                                                xmlns="http://www.w3.org/2000/svg"
-                                                            >
-                                                                <path
-                                                                    fill-rule="evenodd"
-                                                                    d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                                                                    clip-rule="evenodd"
-                                                                ></path>
-                                                            </svg>
-                                                        </div>
+                                            <div className='flex p-1 lg:w-1/4 w-screen lg:pl-8'>
+                                                <div className='flex flex-col w-full'>
+                                                    <div className='flex flex-col rounded-lg border mr-4 w-full mb-6'>
+                                                        <div className="relative w-full border-b p-1">
+                                                            <input
+                                                                datepicker
+                                                                datepicker-autohide
+                                                                type="text"
+                                                                className="text-gray-900 sm:text-sm border-none block w-full p-2.5 focus:ring-0 focus:ring-offset-0"
+                                                                placeholder="Select date"
+                                                                // onSelect={(e) => console.log(e.target.value)}
+                                                                onSelect={(e) => setDate(e.target.value)}
+                                                                // onClick={(e) => dobHandler(e)}
+                                                                // onClick={(e) => console.log(e.target.value)}
+                                                                // onChange={(e) => console.log(e)}
+                                                                id="datepickerId"
+                                                            ></input>
+                                                            <div className="flex absolute inset-y-0 right-0 items-center pr-3 pointer-events-none">
+                                                                <svg
+                                                                    aria-hidden="true"
+                                                                    class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                                                                    fill="currentColor"
+                                                                    viewBox="0 0 20 20"
+                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                >
+                                                                    <path
+                                                                        fill-rule="evenodd"
+                                                                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                                                                        clip-rule="evenodd"
+                                                                    ></path>
+                                                                </svg>
+                                                            </div>
 
+                                                        </div>
+                                                        <div>
+                                                            <input type="number" id="first_name" class="text-gray-900 text-sm rounded-lg border-none focus:ring-0  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Num nights" required
+                                                                onChange={(e) => setNights(e.target.value)}></input>
+                                                        </div>
                                                     </div>
                                                     <div>
-                                                        <input type="number" id="first_name" class="text-gray-900 text-sm rounded-lg border-none focus:ring-0  block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white" placeholder="Num nights" required
-                                                        onChange={(e) => setNights(e.target.value)}></input>
+                                                        {!loading && (
+                                                            <Button size="lg" className='w-full' onClick={() => {setTrackerItem(true); setLoading(true)}}>
+                                                                {trackerSet ? 'Tracker Set' : 'Track'}
+                                                            </Button>
+                                                        )}
+                                                        {loading && (
+                                                        <Button size="lg" className='w-full' onClick={() => setTrackerItem(true)}>
+                                                            <Spinner aria-label="Spinner button example" />
+                                                            <span className="pl-3">
+                                                                Setting Tracker
+                                                            </span>
+                                                        </Button>
+                                                        )}
                                                     </div>
                                                 </div>
-                                                <div>
-                                                    <Button size="lg" className='w-full' onClick={() => setTrackerItem(true)}>
-                                                        Track
-                                                    </Button>
-                                                </div>
                                             </div>
-                                        </div>
                                         )}
                                     </div>
                                 </div>
