a357f477d9bccbb1b36482d869e895fcb79a847e
Logan Orr <logan_orr@icloud.com>
2023-04-06 19:23:52 -0600

    Removing node modules

--- service/db/ddbclient.js
+++
@@ -1,6 +0,0 @@
-const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-// Set the AWS Region.
-const REGION = "us-east-1"; //e.g. "us-east-1"
-// Create an Amazon DynamoDB service client object.
-const ddbClient = new DynamoDBClient({ region: REGION });
-module.exports = { ddbClient }
